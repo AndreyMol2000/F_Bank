294fd32aec470c42a46133b4a9d182f27eb67865
AndreyMol2000 <devildontcrays@gmail.com>
2025-09-02 01:25:51 +0300

    исправил ошибки

--- conftest.py
+++ conftest.py
@@ -1,34 +1,33 @@
+# conftest.py
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import subprocess
 import time
+import os
 
 @pytest.fixture(scope="module")
-def browser():
+def serve_site():
+    """Поднимаем HTTP сервер на 8000 из папки dist"""
+    process = subprocess.Popen(
+        ["python3", "-m", "http.server", "8000"],
+        cwd=os.path.join(os.path.dirname(__file__), "..", "dist"),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
+    )
+    time.sleep(2)  # ждём пока сервер стартанёт
+    yield
+    process.terminate()
+    process.wait()
+
+
+@pytest.fixture(scope="module")
+def browser(serve_site):
+    """Запускаем Chrome в headless режиме"""
     options = Options()
-    options.add_argument("--headless")  # режим без окна, работает на GitHub
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     driver = webdriver.Chrome(options=options)
     yield driver
     driver.quit()
-
-    @pytest.fixture(scope="module" , autouse=True)
-    def serve_site():
-        import subprocess
-        import time
-        import os
-
-        process = subprocess.Popen(
-            ["python3", "-m" , "http.server" , "8000"],
-            cwd= os.path.join(os.path.dirname(__file__), ".." , 'dist') , 
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE
-        )
-
-        time.sleep(2)
-        yield
-        process.terminate()
-        process.wait()
-
-        
--- test_script.py
+++ test_script.py
@@ -1,75 +1,36 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.keys import Keys
+# test_script.py
 import time
 import math
+from selenium.webdriver.common.by import By
 
-options = Options()
-
-driver = webdriver.Chrome(options=options)
-driver.get("http://localhost:8000/?balance=30000&reserved=20001")
-
-def test_input_number_rub(type , number):
-    click_block = driver.find_element(By.ID , type)
-    click_block.click()
-    number_input = driver.find_element(By.CSS_SELECTOR, "[type = text]")
+def test_input_number_rub(browser, block_id, number):
+    browser.find_element(By.ID, block_id).click()
+    number_input = browser.find_element(By.CSS_SELECTOR, "[type=text]")
     number_input.clear()
-    number_input.send_keys(number)
-'''
-test_input_number_rub("rub-sum" , "222222222222222")
-test_input_number_rub("usd-sum" , "2222222222222222")
-test_input_number_rub("euro-sum" , "22222222222222222")
+    number_input.send_keys(str(number))
 
-'''
-def test_input_summ_rub(type , num):
-    test_input_number_rub(type , "2222222222222222")
-    summ = driver.find_element(By.CSS_SELECTOR , "input[placeholder='1000']")
+
+def test_input_summ_rub(browser, block_id, num):
+    test_input_number_rub(browser, block_id, "2222222222222222")
+    summ = browser.find_element(By.CSS_SELECTOR, "input[placeholder='1000']")
     summ.clear()
-    summ.send_keys(num)
-    summ.clear()
-
-'''
-test_input_summ_rub("rub-sum" , "10000")
-test_input_summ_rub("rub-sum","9099")
-test_input_summ_rub("rub-sum" ,"-10")
-test_input_summ_rub("rub-sum" ,"0")
+    summ.send_keys(str(num))
 
 
-test_input_summ_rub("usd-sum" , "10000")
-test_input_summ_rub("usd-sum","9099")
-test_input_summ_rub("usd-sum" ,"-10")
-test_input_summ_rub("usd-sum" ,"0")
-
-test_input_summ_rub("euro-sum" , "10000")
-test_input_summ_rub("euro-sum","9099")
-test_input_summ_rub("euro-sum" ,"-10")
-test_input_summ_rub("euro-sum" ,"0")
-'''
-def test_comission(type ,  summa  ):
-    test_input_summ_rub(type , summa)
-    comission = driver.find_element(By.ID , "comission")
-    if int(comission.text) == summa * 0.1:
-        print("hi")
+def test_comission(browser, block_id, summa):
+    summa = int(summa)
+    test_input_summ_rub(browser, block_id, summa)
+    comission = browser.find_element(By.ID, "comission")
+    commission_value = int(comission.text)
+    
+    expected = math.floor(summa * 0.1)
+    if commission_value == expected:
+        print(f"{block_id} {summa}: hi")
     else:
-        print("ошибка")
-
-'''
-test_comission("rub-sum" , 10000)
-test_comission("rub-sum",9099)
-test_comission("rub-sum" ,-10)
-test_comission("rub-sum" ,0)
+        print(f"{block_id} {summa}: ошибка! ожидалось {expected}, получили {commission_value}")
 
 
-test_comission("usd-sum" , 10000)
-test_comission("usd-sum",9099)
-test_comission("usd-sum" ,-10)
-test_comission("usd-sum" ,0)
-
-test_comission("euro-sum" , 10000)
-test_comission("euro-sum",9099)
-test_comission("euro-sum" ,-10)
-test_comission("euro-sum" ,0)
-'''
+def test_all_comissions(browser):
+    for currency in ["rub-sum", "usd-sum", "euro-sum"]:
+        for value in [10000, 9099, -10, 0]:
+            test_comission(browser, currency, value)
